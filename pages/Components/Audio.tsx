183cf39f891aeca8febe24e6c2124be23216d7b5
professionalowo <martin.brandl1307@gmail.com>
2024-01-22 12:02:57 +0100

    added warning if browser doesnt support source file

--- pages/Components/Audio.tsx
+++ pages/Components/Audio.tsx
@@ -1,5 +1,6 @@
 export function Audio({ src, type, ...props }: Hono.AudioHTMLAttributes) {
     return <audio {...props}>
         <source src={src} type={type}></source>
+        Your browser doesn't support the audio tag
     </audio>
 }
